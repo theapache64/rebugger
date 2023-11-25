597d27efc0055579c97084f8310fe13371624a90
theapache64 <theapache64@gmail.com>
2023-11-25 15:31:39 +0530

    🌟 add auto func name detection for JS

--- rebugger/src/jsMain/kotlin/com/theapache64/rebugger/Rebugger.kt
+++ rebugger/src/jsMain/kotlin/com/theapache64/rebugger/Rebugger.kt
@@ -1,5 +1,14 @@
 package com.theapache64.rebugger
 
+
+
 internal actual fun findComposableName() : String?{
-    return null // TODO
+    val error = Error("dummy")
+    return error.stackTraceToString()
+        .split("\n")
+        .getOrNull(3)
+        ?.split("$")
+        ?.getOrNull(0)
+        ?.split(" ")
+        ?.lastOrNull()
 }
