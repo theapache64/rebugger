f6d01e9afd78d83cd9bca29940d5952459100e2c
theapache64 <theapache64@gmail.com>
2024-04-03 01:42:36 +0530

    🐛 fix dex issue

--- composeApp/src/androidMain/kotlin/com/github/theapache64/rebugger/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/github/theapache64/rebugger/MainActivity.kt
@@ -1,25 +1,11 @@
 package com.github.theapache64.rebugger
 
-import android.app.Application
-import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.theapache64.rebuggersample.RebuggerSample
 
-class AndroidApp : Application() {
-    companion object {
-        lateinit var INSTANCE: AndroidApp
-    }
-
-    override fun onCreate() {
-        super.onCreate()
-        INSTANCE = this
-    }
-}
-
-class AppActivity : ComponentActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
